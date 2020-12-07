dd8603d3b4956e14569cc6e69fb8570a899945a1
alen <andrewsunup@gmail.com>
2020-12-06 22:21:48 -0500

    changed port number to 80

--- paralism_test.py
+++ paralism_test.py
@@ -1,27 +1,28 @@
 import requests
 
-import multiprocessing 
-import time 
-  
-  
-class Process(multiprocessing.Process): 
-	def __init__(self, id, imgpath): 
-		super(Process, self).__init__() 
+import multiprocessing
+import time
+
+
+class Process(multiprocessing.Process):
+	def __init__(self, id, imgpath):
+		super(Process, self).__init__()
 		self.id = id
 		self.imgpath = imgpath
-				 
+
 	def run(self):
 		global global_start_time
-		print("I'm the process with id: {}".format(self.id)) 
+		# global url
+		print("I'm the process with id: {}".format(self.id))
 		start_time = time.time()
 		r = requests.post(url, files={"file": open(self.imgpath, "rb")})
 		print("process {} finished, used {} s, global time passed {} s".format(self.id, time.time() - start_time, time.time() - global_start_time))
 		print(r)
-  
-url = "http://pcvm1-20.instageni.osu.edu:5555/predict"
 
-base_path = "/Users/runqitian/Workspace/Fall2020/CS655/FinalProject/"
-imgs = [base_path + "img1.jpg", base_path + "img2.png", base_path + "img3.jpg", base_path + "img4.jpeg", base_path + "img5.jpg"]
+url = "http://pcvm1-4.geni.case.edu:5555/predict"
+
+base_path = "/Users/alen/Desktop/test_images/"
+imgs = [base_path + "1.jpeg", base_path + "2.jpeg", base_path + "3.jpeg", base_path + "4.jpeg", base_path + "5.jpeg"]
 
 global_start_time = time.time()
 if __name__ == "__main__":
