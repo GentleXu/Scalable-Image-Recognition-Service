cacee5455a95c717c29aec01ac1a30a0596a07a3
runqitian <runqitian@Runqis-MacBook-Pro.local>
2020-12-06 19:31:55 -0500

    finsh docker part

--- paralism_test.py
+++ paralism_test.py
@@ -5,30 +5,25 @@ import time
   
   
 class Process(multiprocessing.Process): 
-    def __init__(self, id, imgpath): 
-        super(Process, self).__init__() 
-        self.id = id
-        self.imgpath = imgpath
-                 
-    def run(self): 
-        print("I'm the process with id: {}".format(self.id)) 
-        r = requests.post(url, files={"file": open(self.imgpath, "rb")})
-        print("process {} finished".format(self.id))
-        print(r)
+	def __init__(self, id, imgpath): 
+		super(Process, self).__init__() 
+		self.id = id
+		self.imgpath = imgpath
+				 
+	def run(self):
+		global global_start_time
+		print("I'm the process with id: {}".format(self.id)) 
+		start_time = time.time()
+		r = requests.post(url, files={"file": open(self.imgpath, "rb")})
+		print("process {} finished, used {} s, global time passed {} s".format(self.id, time.time() - start_time, time.time() - global_start_time))
+		print(r)
   
-# if __name__ == '__main__': 
-#     p = Process(0) 
-#     p.start() 
-#     p.join() 
-#     p = Process(1) 
-#     p.start() 
-#     p.join()
-
 url = "http://pcvm1-20.instageni.osu.edu:5555/predict"
 
 base_path = "/Users/runqitian/Workspace/Fall2020/CS655/FinalProject/"
 imgs = [base_path + "img1.jpg", base_path + "img2.png", base_path + "img3.jpg", base_path + "img4.jpeg", base_path + "img5.jpg"]
 
+global_start_time = time.time()
 if __name__ == "__main__":
 	ps = []
 	for i in range(5):
@@ -37,7 +32,3 @@ if __name__ == "__main__":
 
 	for p in ps:
 		p.start()
-	time.sleep(20)
-	# ps[0].join()
-# for p in ps:
-# 	p.start()
